5d18a690f26fb412e67df16713a21988b4d778b5
minotad <darwin.minota@wolox.co>
2021-05-26 16:25:33 -0500

    services-promises-witter

---
+++ WTraining/app/services/weet.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const axios = require('axios');
+
+const logger = require('../logger');
+
+const getWeet = () =>
+  axios
+    .get('https://geek-jokes.sameerkumar.website/api?format=json')
+    .then(res => res.data)
+    .catch(e => logger.error(e));
+
+module.exports = {
+  getWeet
+};
