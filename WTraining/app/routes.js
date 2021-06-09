71ff36f4e2a89cea31434b2dc59b885e5364f6c0
minotad <darwin.minota@wolox.co>
2021-06-09 11:40:45 -0500

    change user_validation to userValidation

--- WTraining/app/routes.js
+++ WTraining/app/routes.js
@@ -1,9 +1,9 @@
 const { healthCheck } = require('./controllers/health_check');
 const { createUser } = require('./controllers/user');
-const { user_validation } = require('./schemas/user');
+const { userValidation } = require('./schemas/user');
 const { validateSchema } = require('./middlewares/validations');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/users', [validateSchema(user_validation)], createUser);
+  app.post('/users', [validateSchema(userValidation)], createUser);
 };
