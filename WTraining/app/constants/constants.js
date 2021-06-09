b32da86e1199b7476d08b7029859b1c0962072b2
minotad <darwin.minota@wolox.co>
2021-06-09 10:49:07 -0500

    sign up verification middleware

--- WTraining/app/constants/constants.js
+++ WTraining/app/constants/constants.js
@@ -1,4 +1,2 @@
-exports.VALIDATE_EMAIL = /^(([^<>(),;:\s@"]+(.[^<>(),;:\s@"]+)*)|(.+"))@((wolox+.)+[^<>()[,;:\s@"]{2,})$/i;
-exports.VALIDATE_STRING = /^[A-Za-z]+$/i;
 exports.SALT = 10;
-exports.MSG_EMAIL_NOT_UNIQUE = 'Email is already in use.';
+exports.MSG_EMAIL_NOT_UNIQUE = 'Email is already in use';
--- WTraining/app/routes.js
+++ WTraining/app/routes.js
@@ -1,7 +1,9 @@
 const { healthCheck } = require('./controllers/health_check');
 const { createUser } = require('./controllers/user');
+const { user_validation } = require('./schemas/user');
+const { validateSchema } = require('./middlewares/validations');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/users', createUser);
+  app.post('/users', [validateSchema(user_validation)], createUser);
 };
