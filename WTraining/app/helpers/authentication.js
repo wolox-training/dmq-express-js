04d860fd24281590391ebed79422a63d72316703
minotad <84399828+minotad@users.noreply.github.com>
2021-06-10 10:56:05 -0500

    Update authentication.js

--- WTraining/app/helpers/authentication.js
+++ WTraining/app/helpers/authentication.js
@@ -1,6 +1,5 @@
 const { encode } = require('jwt-simple');
 const config = require('../../config').common.jwt;
 
-exports.generateToken = payload => {
-  return encode(payload, config.jwtSecret);
-};
+exports.generateToken = payload => encode(payload, config.jwtSecret);
+
