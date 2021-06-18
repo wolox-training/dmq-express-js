b377b67cc2f3fd59a75d5db30b0fbd5ea1c27335
minotad <darwin.minota@wolox.co>
2021-06-18 15:27:24 -0500

    the method for querying the position is created

--- WTraining/app/helpers/user_level.js
+++ WTraining/app/helpers/user_level.js
@@ -1,7 +1,26 @@
 'use strict';
-const { CEO } = require('../constants/constants');
+const {
+  DEVELOPER,
+  LEAD,
+  TL,
+  EM,
+  HEAD,
+  CEO,
+  IS_DEV,
+  IS_EM,
+  IS_HEAD,
+  IS_LEAD,
+  IS_TL
+} = require('../constants/constants');
 
-exports.userLevel = (points, levels) => {
-  const response = levels.find(item => points >= item.min && points < item.max);
-  return response.level || CEO;
+const getPositionDev = score => (score < IS_DEV ? DEVELOPER : LEAD);
+const getPositionMedium = score => (score >= IS_LEAD && score < IS_TL ? TL : EM);
+const getPositionLeads = score => (score >= IS_HEAD ? CEO : HEAD);
+
+exports.getPosition = score => {
+  let response = '';
+  if (score < IS_LEAD) response = getPositionDev(score);
+  if (score >= IS_LEAD && score < IS_EM) response = getPositionMedium(score);
+  if (score >= IS_EM) response = getPositionLeads(score);
+  return response;
 };
