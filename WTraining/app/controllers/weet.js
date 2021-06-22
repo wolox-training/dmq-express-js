951e9433e42137a03f13aacb56ac8181a4499d63
minotad <darwin.minota@wolox.co>
2021-06-22 09:51:30 -0500

    controller

--- WTraining/app/controllers/weet.js
+++ WTraining/app/controllers/weet.js
@@ -5,6 +5,7 @@ const db = require('../models');
 const { paginationMapper } = require('../mappers/pagination');
 const { countPerPage } = require('../helpers/count');
 const { allWeetsSerializer } = require('../serializers/weet');
+const { SUBSTR_START, SUBSTR_END } = require('../constants/constants');
 const {
   weetRatingDoesNotExist,
   weetRatingExist,
@@ -31,7 +32,7 @@ exports.createWeet = async (req, res, next) => {
   try {
     const userId = req.user.id;
     const weet = await weetService.getWeet();
-    const response = await weetService.createWeet({ content: weet.substr(0, 140), userId });
+    const response = await weetService.createWeet({ content: weet.substr(SUBSTR_START, SUBSTR_END), userId });
     return res.status(201).send(response);
   } catch (e) {
     logger.error(e.ValidationErrorItem);
